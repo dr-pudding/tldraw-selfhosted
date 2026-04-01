9a3c7e232768843631d9f78d875f5c7ba211c9ac
dr-pudding <jackmj@proton.me>
2026-03-31 21:55:44 -0500

    default redirect to /main board

--- src/client/pages/Root.tsx
+++ src/client/pages/Root.tsx
@@ -1,11 +1,11 @@
-import { Navigate } from 'react-router-dom'
-import { uniqueId } from 'tldraw'
-import { getLocalStorageItem, setLocalStorageItem } from '../localStorage'
-import { ReactNode, useEffect, useState } from 'react'
+import { Navigate } from "react-router-dom";
+import { uniqueId } from "tldraw";
+import { getLocalStorageItem, setLocalStorageItem } from "../localStorage";
 
-const myLocalRoomId = getLocalStorageItem('my-local-room-id') ?? 'test-room-' + uniqueId()
-setLocalStorageItem('my-local-room-id', myLocalRoomId)
+const myLocalRoomId =
+    getLocalStorageItem("my-local-room-id") ?? "test-room-" + uniqueId();
+setLocalStorageItem("my-local-room-id", myLocalRoomId);
 
 export function Root() {
-	return <Navigate to={`/${myLocalRoomId}`} />
+    return <Navigate to={"/main"} />;
 }
